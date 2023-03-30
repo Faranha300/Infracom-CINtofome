f17072ecfd3a2549dd8658457ed3d404965a34bc
Fabrício Aranha <fabricioaranha1@gmail.com>
2023-03-30 10:28:59 -0300

    Bug corrigido

--- Entrega 1/receiver.py
+++ Entrega 1/receiver.py
@@ -11,30 +11,29 @@ buf = 1024
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, IN_PORT))
 
+data, addr = sock.recvfrom(1024)
+if data:
+    print("File name:", data.decode())
+    file_name = data.strip()
+
+f = open('novo_arquivo.txt', 'w+b')
+
 while True:
-    data, addr = sock.recvfrom(1024)
-    if data:
-        print("File name:", data.decode())
-        file_name = data.strip()
-
-    f = open('novo_arquivo.txt', 'w+b')
-
-    while True:
-        ready = select.select([sock], [], [], timeout)
-        if ready[0]:
-            data, addr = sock.recvfrom(1024)
-            f.write(data)
-        else:
-            print("%s Finish!" % file_name.decode())
-            f.close()
-            break
+    ready = select.select([sock], [], [], timeout)
+    if ready[0]:
+        data, addr = sock.recvfrom(1024)
+        f.write(data)
+    else:
+        print("%s Finish!" % file_name.decode())
+        f.close()
+        break
         
-    f = open('novo_arquivo.txt', "rb")
+f = open('novo_arquivo.txt', "rb")
+data = f.read(buf)
+while True:
     data = f.read(buf)
-    while True:
-        data = f.read(buf)
-        sock.sendto(data, addr)
-        time.sleep(0.02)  # Give receiver a bit time to save
-        
-        if (not data):
-            break
+    sock.sendto(data, addr)
+    time.sleep(0.02)  # Give receiver a bit time to save
+    
+    if (not data):
+        break
